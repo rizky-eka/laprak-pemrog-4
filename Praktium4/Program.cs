1100fc72270db3039513ab2dd16bdae88bd042c7
Rizky Eka Haryadi <rizky28eka@students.amikom.ac.id>
2023-04-04 05:36:15 +0700

    git commit
    git commit

---
+++ Praktium4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Praktium4;
+
+namespace Praktium4
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            mobil mobill = new mobil();
+
+
+            mobill.warna = "silver";
+            mobill.jumlah_pintu = 2;
+            mobill.merek = "Avanza";
+            mobill.model = "MPV";
+            mobill.tahun_keluaran = 2019;
+
+            mobill.Gas(300);
+            mobill.klakson("Klakson udara");
+            mobill.TampilkanInfo();
+        }
+    }
+}
---
+++ Praktium4/mobil.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+    
+namespace Praktium4
+{
+	internal class mobil
+	{
+        // Field
+        private string _warna { get; set; }
+        private int _jumlah_pintu { get; set; }
+        private string _merek { get; set; }
+        private string _model { get; set; }
+        private int _tahun_keluaran { get; set; }
+
+        // properties
+        public string warna
+        {
+            get
+            {
+                return _warna;
+            }
+            set
+            {
+                _warna = value;
+            }
+        }
+        public int jumlah_pintu
+        {
+            get
+            {
+                return _jumlah_pintu;
+            }
+            set
+            {
+                jumlah_pintu = value;
+            }
+        }
+        public string merek
+        {
+            get
+            {
+                return _merek;
+            }
+            set
+            {
+                _merek = value;
+            }
+        }
+        public string model
+        {
+            get
+            {
+                return _model;
+            }
+            set
+            {
+                _model = value;
+            }
+        }
+        public int tahun_keluaran
+        {
+            get
+            {
+                return _tahun_keluaran;
+
+            }
+            set
+            {
+                _tahun_keluaran = value;
+            }
+        }
+        // method
+        public void Gas(int kecepatan)
+        {
+            Console.WriteLine("Mobil {0} berjalan dengan kecepatan {1} km/jam", model, kecepatan);
+            Console.ReadKey();
+        }
+        public void klakson(string suara)
+        {
+            Console.WriteLine("0", suara);
+            Console.ReadKey();
+        }
+        public void TampilkanInfo()
+        {
+            Console.WriteLine("Mobil saya berwarna {0}, merek {1}, model {2} keluaran tahun {3) dengan jumlah pintu sebanyak {4}", warna, merek, model, tahun_keluaran, jumlah_pintu);
+            Console.ReadKey();
+        }
+    }
+
+}
+}
+
